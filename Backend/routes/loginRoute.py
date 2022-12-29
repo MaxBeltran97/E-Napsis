0d2971d3817d6257269fa2a1ebaf5bdbb68120c0
Diego <whoisdiego.tm@gmail.com>
2022-12-28 23:36:47 -0300

    Docker

--- Backend/routes/loginRoute.py
+++ Backend/routes/loginRoute.py
@@ -1,13 +1,15 @@
 import flask
+import jwt
 from flask import request
 from database.config import Config
 from models.user import *
 from models.userRole import *
-from flask_jwt_extended import create_access_token, get_jwt_identity, JWTManager, verify_jwt_in_request
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
+from werkzeug.security import check_password_hash, generate_password_hash
 from email.message import EmailMessage
 import ssl
 import smtplib
+from datetime import timedelta
 
 
 
@@ -21,11 +23,8 @@ login = flask.Blueprint('login', __name__)
 def signup_post():
     try:
         user_requested = request.json['username']
-        print(user_requested)
         password = request.json['password']
         isEmail = False
-
-        #now = datetime.now() + timedelta(days=1)
 
         # verifica si es username o email
         for i in user_requested:
@@ -37,10 +36,10 @@ def signup_post():
 
             # verifica que exista el usuario con esa contraseña
             if user and check_password_hash(user.password, password):
-                # access_token = create_access_token(
-                #     identity=user_requested, expires_delta=now)
                 access_token = create_access_token(
-                    identity=user_requested)
+                    identity=user_requested, expires_delta=timedelta(days=1))
+
+                refresh_token = create_refresh_token(identity=user_requested)
 
                 data = user.serialize()
                 del data['password']
@@ -62,20 +61,24 @@ def signup_post():
                 # access_token = create_access_token(
                 #     identity=user_requested, expires_delta=now)
                 access_token = create_access_token(
-                    identity=user_requested)
+                    identity=user_requested, expires_delta=timedelta(days=1))
+
+                refresh_token = create_refresh_token(identity=user_requested)
+
                 data = user.serialize()
                 del data['password']
 
                 return {
                     "ok": True,
                     "user": data,
-                    "token": access_token
+                    "token": access_token,
+                    "refresh": refresh_token
                 }
             else:
                 return {
                     "ok": False,
                     "msg": "Usuario y/o Contraseña Incorrectos"
-                }
+                }, 401
 
     except Exception as e:
         print(e)
@@ -103,6 +106,7 @@ def get_role(_id):
             "ok": False,
             "msg": "Error al obtener el rol"
         }, 500
+
 
 @login.route('/api/mailSending/<_id>', methods=['GET'])
 def send_mail(_id):
@@ -143,3 +147,45 @@ def send_mail(_id):
             "ok": False,
             "msg": "Error al mandar el correo"
         }, 500
+
+
+@login.route('/api/login/refresh', methods=["POST"])
+@jwt_required(refresh=True)
+def refresh_users_token():
+    identity = get_jwt_identity()
+    access = create_refresh_token(identity=identity)
+    return {
+            "ok": True,
+            "access": access
+        }, 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @login.route('/api/login/test', methods=['GET'])
+# @jwt_required()
+# def test():
+    
+#     try:
+#         access_token_valid = get_jwt_identity()
+#         print(access_token_valid)
+
+#         return {
+#             "ok": True,
+#             "msg": "Good"
+#         }
+#     except Exception as e:
+#         print(e)
+#         return {
+#             "ok": False,
+#             "msg": "Error en el test"
+#         }, 500
